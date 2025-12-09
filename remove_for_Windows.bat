88d5f2d4421f837fa44fefad6df5b9004f3cd35b
Chizhov Vitaliy <chizhov.vitaliy@inbox.ru>
2025-12-09 18:49:28 +0300

    add remove for windows

---
+++ remove_for_Windows.bat
@@ -0,0 +1,9 @@
+@echo off
+echo delete node-exporter
+kubectl delete -f k8s/monitoring/node-exporter/ -n kube-system
+echo delete namespace test-dbo-system
+kubectl delete namespace test-dbo-system
+
+echo Press any key to exit...
+pause >nul
+exit /
